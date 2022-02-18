54f250f90913d76d0a63af784bb12c180c4028c3
linda <lindamcc98@live.com>
2022-02-18 15:43:22 +0100

    icon

--- native-app/components/screens/Home.js
+++ native-app/components/screens/Home.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Text, SafeAreaView} from 'react-native';
-import styles from '../../styles/styles';
+import { Text, SafeAreaView } from "react-native";
+import styles from "../../styles/styles";
 import ButtonContainer from "../ButtonContainer";
 
-const HomeScreen = ({navigation}) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.h1}>Welcome home!</Text>
-            <ButtonContainer navigation={navigation}/>
-        </SafeAreaView>
-    );
-}
+const HomeScreen = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.h1}>Welcome home!</Text>
+      <ButtonContainer navigation={navigation} />
+    </SafeAreaView>
+  );
+};
 
 export default HomeScreen;
-
--- native-app/routes/HomeStack.js
+++ native-app/routes/HomeStack.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../components/screens/Home';
-import Camera from '../components/screens/Camera';
-import Bluetooth from '../components/screens/Bluetooth';
-import {Icons} from 'react-native-fontawesome';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../components/screens/Home";
+import Camera from "../components/screens/Camera";
+import Bluetooth from "../components/screens/Bluetooth";
+
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
+export default function Navigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerLeft: () => <Ionicons name="paper-plane" size={32} />,
+          headerStyle: {
+            backgroundColor: "#e5e4df",
+          },
 
-export default function Navigator(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator 
-                initialRouteName="Home"
-                screenOptions={{headerStyle: {
-                    backgroundColor: '#e5e4df'
-                },
-                headerTintColor: '#000',
-                headerTitleStyle: {
-                    fontWeight: 'normal',
-                },
-                }}
-                >
-                <Stack.Screen 
-                    name='Home' 
-                    component={Home}
-                    options={{title: 'Native App'}}
-                />
-                <Stack.Screen name='Camera' component={Camera}/>
-                <Stack.Screen name='Bluetooth' component={Bluetooth}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "normal",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Native App" }}
+        />
+        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Bluetooth" component={Bluetooth} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
